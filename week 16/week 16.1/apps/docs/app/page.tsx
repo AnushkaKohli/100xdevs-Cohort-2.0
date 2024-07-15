2e32bad2751ff2d1e696bcb7e9dea7e97db3cb58
Anushka <anushkakohli2021@gmail.com>
2024-07-15 22:38:45 +0530

    custom comp turborepo

--- week 16/week 16.1/apps/docs/app/page.tsx
+++ week 16/week 16.1/apps/docs/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import Admin from "@repo/ui/admin";
 
-export default function Home() {
+export default function Home () {
   return (
     <div className={styles.page}>
+      <Admin />
       <main className={styles.main}>
         <Image
           className={styles.logo}
---
+++ week 16/week 16.1/packages/ui/src/admin.tsx
@@ -0,0 +1,9 @@
+const Admin = () => {
+    return (
+        <h1>
+            Hi from Admin Team
+        </h1>
+    )
+}
+
+export default Admin
