e48ee72d84fdecd628f9e4903ee0a3f1a6bbd3e2
anushka0kohli <anushka.kohli@dgp.pinnacleinfotech.com>
2024-07-17 14:55:03 +0530

    coomon module in turborepo

--- week 16/week 16.1/apps/backend/src/index.ts
+++ week 16/week 16.1/apps/backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
-
+import { VALUE } from "@repo/common/config"
 const app = express();
 
+console.log("Value: ", VALUE);
 app.get("/", (req, res) => {
-    res.json({ message: "Healthy server" });
+    res.json({ message: "Healthy server", "value": VALUE });
 })
 
 app.listen(3003)
+
